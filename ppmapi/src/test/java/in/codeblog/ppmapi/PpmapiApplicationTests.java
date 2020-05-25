0a0675c3682b9057ba52f50cfa4b2808a8097271
Mehul Khoiwal <mhlkhoiwal@gmail.com>
2020-05-25 16:13:53 +0530

    Project with Backlog and projectTasks

--- ppmapi/src/test/java/in/codeblog/ppmapi/PpmapiApplicationTests.java
+++ ppmapi/src/test/java/in/codeblog/ppmapi/PpmapiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.yash.ppmtoolapi;
+package in.codeblog.ppmapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PpmtoolapiApplicationTests {
+class PpmapiApplicationTests {
 
 	@Test
 	void contextLoads() {
